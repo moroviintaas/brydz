8206e60f06d741adfdeda4ce16291ee3441a6ae3
moroviintaas <moroviintaas@gmail.com>
2025-10-16 19:20:29 +0200

    Sketch for model config structure

--- brydz_model/src/bin/model.rs
+++ brydz_model/src/bin/model.rs
@@ -1,3 +1,58 @@
-fn main() {
+use std::path::PathBuf;
+use clap::Args;
+use log::LevelFilter;
+use brydz_model::options::contract::ModelConfig;
 
+#[derive(Debug, Args)]
+pub struct RunCli{
+
+    #[arg(short = 'l', long = "log", default_value_t= LevelFilter::Info)]
+    pub log_level: LevelFilter,
+    #[arg(short = 'c', long = "log_core", default_value_t= LevelFilter::Error)]
+    pub brydz_core_log_level: LevelFilter,
+    #[arg(short = 'a', long = "log_amfi", default_value_t= LevelFilter::Error)]
+    pub amfi_log_level: LevelFilter,
+    #[arg(short = 'r', long = "log_amfi-rl", default_value_t= LevelFilter::Error)]
+    pub amfiteatr_rl_log_level: LevelFilter,
+
+    #[arg(long = "log_file")]
+    pub log_file: Option<PathBuf>,
+
+    #[arg(short = 'f', long = "config", help = "File with model configuration")]
+    pub config_path: Option<PathBuf>,
+}
+
+pub fn setup_logger(options: &RunCli) -> Result<(), fern::InitError> {
+    let dispatch  = fern::Dispatch::new()
+
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        //.level(log_level)
+        .level_for("brydz_model", options.log_level)
+        .level_for("brydz_core", options.brydz_core_log_level)
+        .level_for("amfiteatr_rl", options.amfiteatr_rl_log_level)
+        .level_for("amfiteatr_core", options.amfi_log_level);
+
+    match &options.log_file{
+        None => dispatch.chain(std::io::stdout()),
+        Some(f) => dispatch.chain(fern::log_file(f)?)
+    }
+        .apply()?;
+    Ok(())
+}
+fn main() -> anyhow::Result<()> {
+
+    let config  = ModelConfig::default();
+
+    let s = serde_yaml::to_string(&config)?;
+
+    println!("{}",s);
+    Ok(())
 }
--- brydz_model/src/options/contract.rs
+++ brydz_model/src/options/contract.rs
@@ -1,11 +1,12 @@
 use std::path::PathBuf;
 use clap::{Args, ValueEnum};
+use log::LevelFilter;
 use serde::{Deserialize, Serialize};
 use amfiteatr_rl::policy::{ConfigA2C, ConfigPPO};
+use brydz_core::player::side::SideMap;
 
 
-
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Default, Debug)]
 pub struct PolicyOuterConfig{
     pub var_store_load: Option<PathBuf>,
     pub var_store_save: Option<PathBuf>,
@@ -13,21 +14,29 @@ pub struct PolicyOuterConfig{
 
 }
 
-#[derive(Copy, Clone, Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
 pub enum AgentPolicyInnerConfig{
     PPO(ConfigPPO),
     A2C(ConfigA2C)
 }
 
-#[derive(Copy, Clone, Serialize, Deserialize, ValueEnum)]
+impl Default for AgentPolicyInnerConfig{
+    fn default() -> Self {
+        Self::PPO(ConfigPPO::default())
+    }
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize, ValueEnum, Default, Debug)]
 pub enum InformationSetSelection{
     CompleteKnowledge,
     DistributionAssume,
+    #[default]
     Simple,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Default, Debug)]
 pub struct AgentConfig{
+    #[serde(default)]
     pub limit_learn_epochs: Option<usize>,
     pub policy: AgentPolicyInnerConfig,
     pub policy_data: PolicyOuterConfig,
@@ -36,18 +45,25 @@ pub struct AgentConfig{
 
 }
 
-#[derive(Clone, Serialize, Deserialize, ValueEnum)]
+#[derive(Clone, Serialize, Deserialize, ValueEnum, Default, Debug)]
 pub enum InformationSetRepresentation{
     Dense,
+    #[default]
     Sparse,
     SparseHistoric
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
 pub struct ModelConfig{
-
+    number_of_epochs: usize,
+    agents: SideMap<AgentConfig>,
+    test_deal_file: Option<PathBuf>,
+    game_deal_biases: Option<PathBuf>,
 }
 
+
+
+/*
 #[derive(Debug, Args)]
 pub struct ContractModelOptions{
 
@@ -55,3 +71,5 @@ pub struct ContractModelOptions{
     #[arg(short = 'p', long = "probability-file", help = "Path to file with probabilities of cards")]
     pub probability_file: Option<PathBuf>,
 }
+
+ */
